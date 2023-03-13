e8e4fabf96ec222bd4fc008db34404ee22c64e45
Bill <mukhanov_nursultan@mail.ru>
2023-03-13 16:22:05 +0400

    other

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,23 @@ const scss = require('gulp-sass')(require('sass'));
 const concat         = require('gulp-concat');
 const browserSync   = require('browser-sync').create();
 
+
+function concat1() {
+    return src(['app/scss/product-information.scss', 'app/scss/style.scss'])
+      .pipe(concat({ path: 'app/scss/style.scss'}))
+      .pipe(dest('./dist/'));
+  };
+
 function styles() {
     return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss({outputStyle: 'expanded'}))
     .pipe(concat('style.min.css'))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
-
 }
+
+
+
 function browsersync(){
     browserSync.init({
         server: {
@@ -19,6 +28,7 @@ function browsersync(){
         }
     });
 }
+
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
     watch(['app/*.html']).on('change', browserSync.reload);
